1e9efd196cf5436e2b842516e74139373a448d62
DmytrivDev <151439615+DmytrivDev@users.noreply.github.com>
2025-08-30 18:13:30 +0300

    Test 4

--- src/modules/about-content/about-content.service.ts
+++ src/modules/about-content/about-content.service.ts
@@ -12,7 +12,7 @@ export class AboutContentService {
       `/about-platform?locale=${locale}&populate[items][populate]=Icon`
     );
 
-    console.log(2111);
+    console.log(3111);
 
     return data;
   }
