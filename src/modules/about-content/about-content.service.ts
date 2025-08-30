cea03a19693e996c8c74bceb28575554569413e9
DmytrivDev <151439615+DmytrivDev@users.noreply.github.com>
2025-08-30 18:07:50 +0300

    Test 2

--- src/modules/about-content/about-content.service.ts
+++ src/modules/about-content/about-content.service.ts
@@ -6,13 +6,13 @@ export class AboutContentService {
   constructor(private readonly strapi: StrapiService) {}
 
   async getAboutContent(locale: string) {
-    console.log(`[AboutContentService] fetching about content, locale=${locale}`);
+    console.log(2222);
 
     const data = await this.strapi.get(
       `/about-platform?locale=${locale}&populate[items][populate]=Icon`
     );
 
-    console.log(`[AboutContentService] received data keys:`, Object.keys(data || {}));
+    console.log(1111);
 
     return data;
   }
