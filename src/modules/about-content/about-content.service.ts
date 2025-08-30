9f1ebe4a91fd895d17a06e411b3b381c4cc24218
DmytrivDev <151439615+DmytrivDev@users.noreply.github.com>
2025-08-30 18:23:23 +0300

    Test F

--- src/modules/about-content/about-content.service.ts
+++ src/modules/about-content/about-content.service.ts
@@ -6,14 +6,9 @@ export class AboutContentService {
   constructor(private readonly strapi: StrapiService) {}
 
   async getAboutContent(locale: string) {
-    console.log(2222);
-
     const data = await this.strapi.get(
       `/about-platform?locale=${locale}&populate[items][populate]=Icon`
     );
-
-    console.log(5111);
-
     return data;
   }
 }
