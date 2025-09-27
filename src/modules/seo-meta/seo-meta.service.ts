2fcf55047c6630fff6d35ab1e52c312ed287690c
DmytrivDev <151439615+DmytrivDev@users.noreply.github.com>
2025-09-27 13:24:47 +0300

    Fix

--- src/modules/seo-meta/seo-meta.service.ts
+++ src/modules/seo-meta/seo-meta.service.ts
@@ -1,55 +1,28 @@
-import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
-import fetch from 'node-fetch';
-import { CreateAmbassadorDto } from './dto/create-ambassador.dto';
+// seo-meta.service.ts
+import { Injectable } from "@nestjs/common";
+import { StrapiService } from "../../services/strapi.service";
 
 @Injectable()
-export class AmbassadorsService {
-  async processApplication(data: CreateAmbassadorDto) {
-    try {
-      // 1. Надсилаємо у Telegram
-      await this.sendToTelegram(data);
+export class SeoMetaService {
+  constructor(private readonly strapi: StrapiService) {}
 
-      // 2. Надсилаємо у Strapi
-      await this.sendToStrapi(data);
+  async getSeoMeta(page: string, locale: string) {
+    let collection = "";
 
-      return { success: true };
-    } catch (err) {
-      console.error(err);
-      throw new HttpException('Помилка при обробці заявки', HttpStatus.BAD_REQUEST);
+
+    if (page === "home") {
+      collection = `${page}-seo-meta`; // home-seo-meta, about-seo-meta
+    } else {
+      collection = `${page}-page`;
     }
-  }
+    const componentName = `${page.charAt(0).toUpperCase()}${page.slice(
+      1
+    )}SeoMeta`; // HomeSeoMeta, AboutSeoMeta
 
-  private async sendToTelegram(data: CreateAmbassadorDto) {
-    const TOKEN = process.env.TELEGRAM_TOKEN;
-    const CHAT_ID = process.env.TELEGRAM_CHAT_ID;
-    const URI_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+    const res = await this.strapi.get(
+      `/${collection}?locale=${locale}&populate=${componentName}.shareImage`
+    );
 
-    const message = `
-🔔 Нова заявка Time2Fest
-👤 Ім’я: ${data.name}
-🌍 Країна: ${data.country}
-📞 Контакт: ${data.contactMethod} → ${data.contactLink}
-📝 Мотивація: ${data.motivation}
-    `;
-
-    await fetch(URI_API, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ chat_id: CHAT_ID, text: message }),
-    });
-  }
-
-  private async sendToStrapi(data: CreateAmbassadorDto) {
-    const STRAPI_URL = process.env.STRAPI_URL;
-    const STRAPI_TOKEN = process.env.STRAPI_TOKEN;
-
-    await fetch(`${STRAPI_URL}/api/ambassadors`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${STRAPI_TOKEN}`,
-      },
-      body: JSON.stringify({ data }),
-    });
+    return res;
   }
 }
