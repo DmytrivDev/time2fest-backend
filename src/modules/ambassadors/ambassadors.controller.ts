5881edb929ec2ac73ee25bd14c2fcf6ee81ca718
DmytrivDev <151439615+DmytrivDev@users.noreply.github.com>
2025-09-27 15:45:28 +0300

    Test 1

--- src/modules/ambassadors/ambassadors.controller.ts
+++ src/modules/ambassadors/ambassadors.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Post, Body } from '@nestjs/common';
-import { AmbassadorsService } from './ambassadors.service';
-import { CreateAmbassadorDto } from './dto/create-ambassador.dto';
+import { Controller, Post, Body } from "@nestjs/common";
+import { AmbassadorsService } from "./ambassadors.service";
+import { CreateAmbassadorDto } from "./dto/create-ambassador.dto";
 
-@Controller('ambassadors')
+@Controller("ambassadors")
 export class AmbassadorsController {
   constructor(private readonly ambassadorsService: AmbassadorsService) {}
 
   @Post()
   async create(@Body() data: CreateAmbassadorDto) {
+    console.log("👉 RAW BODY:", data); // ✅ тепер бачиш, що реально прилітає
     return this.ambassadorsService.processApplication(data);
   }
 }
