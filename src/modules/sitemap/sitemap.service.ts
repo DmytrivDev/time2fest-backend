1f703097a63b11a7f4ac29c311c9cf3b42c8b60d
DmytrivDev <151439615+DmytrivDev@users.noreply.github.com>
2025-09-28 14:56:59 +0300

    ва

--- src/modules/sitemap/sitemap.service.ts
+++ src/modules/sitemap/sitemap.service.ts
@@ -11,7 +11,7 @@ export class SitemapService {
     // 📌 1. Статичні сторінки
     const staticPages = [
       { path: "", changefreq: "daily", priority: 1.0 }, // головна
-      { path: "privacy", changefreq: "daily", priority: 1.0 },
+      { path: "privacy", changefreq: "monthly", priority: 0.2 },
     ];
 
     const locales = ["en", "uk", "es", "fr"]; // можна розширити
