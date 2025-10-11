34e8ba830993d3c26958e9aed272601f8100d6d7
DmytrivDev <151439615+DmytrivDev@users.noreply.github.com>
2025-10-11 17:10:20 +0300

    Ambass full

--- src/modules/countries/countries.service.ts
+++ src/modules/countries/countries.service.ts
@@ -1,24 +1,95 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, InternalServerErrorException } from "@nestjs/common";
 import { StrapiService } from "../../services/strapi.service";
+import { AxiosError } from "axios";
 
 @Injectable()
 export class CountriesService {
   constructor(private readonly strapi: StrapiService) {}
 
   async getCountry(code?: string, slug?: string, locale = "uk") {
-    const qs = new URLSearchParams();
-    qs.set("locale", locale);
-    qs.set("populate", "*");
+    try {
+      const params = new URLSearchParams();
+      params.set("locale", locale);
 
-    // ---- Додаємо фільтр по коду або по слагу ----
-    if (code) {
-      qs.set("filters[CountryCode][$eq]", code.toUpperCase());
-    }
-    if (slug) {
-      qs.set("filters[slug][$eq]", slug.toLowerCase());
-    }
+      // --- ❗ Важливо: додаємо вкладений populate як окремі поля ---
+      params.set("populate[ambassadors][populate][0]", "Photo");
+      params.set("populate[ambassadors][populate][1]", "Video");
+      params.set("populate[ambassadors][populate][2]", "SocialLinks");
+      params.set("populate[time_zones]", "true");
+      params.set("populate[TimezoneDetail]", "true");
+      params.set("populate[Background]", "true");
 
-    const url = `/countries?${qs.toString()}`;
-    return this.strapi.get(url);
+      // --- Фільтри ---
+      if (code) params.set("filters[CountryCode][$eq]", code.toUpperCase());
+      if (slug) params.set("filters[slug][$eq]", slug.toLowerCase());
+
+      const url = `/countries?${params.toString()}`;
+      console.log("🌍 Fetching country from Strapi:", url);
+
+      const resp: any = await this.strapi.get(url);
+      const data = resp?.data ?? resp;
+      if (!Array.isArray(data) || !data.length) return [];
+
+      return data.map((item: any) => {
+        const attrs = item.attributes ?? item;
+
+        // 🖼️ Фонове зображення
+        const bg =
+          attrs.Background?.data?.attributes ?? attrs.Background ?? null;
+        const backgroundUrl = bg?.url ?? null;
+
+        // 🕺 Амбасадори з вкладеними медіа
+        const ambassadors = Array.isArray(attrs.ambassadors)
+          ? attrs.ambassadors.map((a: any) => {
+              const amb = a.attributes ?? a;
+              return {
+                id: amb.id ?? a.id,
+                slug: amb.slug,
+                name: amb.Name ?? "",
+                description: amb.Description ?? "",
+                fullDescription: amb.FullDescription ?? "",
+                languages: amb.Languages ?? "",
+                gender: amb.Gender ?? "",
+                photo:
+                  amb.Photo?.data?.attributes?.url ?? amb.Photo?.url ?? null,
+                video:
+                  amb.Video?.data?.attributes?.url ?? amb.Video?.url ?? null,
+                socialLinks: Array.isArray(amb.SocialLinks)
+                  ? amb.SocialLinks.map((link: any) => ({
+                      name: link?.Name ?? "",
+                      link: link?.Link ?? "",
+                    }))
+                  : [],
+                createdAt: amb.createdAt,
+                updatedAt: amb.updatedAt,
+                locale: amb.locale ?? locale,
+              };
+            })
+          : [];
+
+        return {
+          id: item.id,
+          CountryName: attrs.CountryName ?? "",
+          CountryCode: attrs.CountryCode ?? "",
+          CountryDesc: attrs.CountryDesc ?? "",
+          ShortDesc: attrs.ShortDesc ?? "",
+          slug: attrs.slug ?? "",
+          locale: attrs.locale ?? locale,
+          Background: backgroundUrl,
+          time_zones: attrs.time_zones ?? [],
+          TimezoneDetail: attrs.TimezoneDetail ?? [],
+          ambassadors,
+          createdAt: attrs.createdAt,
+          updatedAt: attrs.updatedAt,
+        };
+      });
+    } catch (err) {
+      const error = err as AxiosError;
+      console.error(
+        "🔥 CountriesService error:",
+        error.response?.data || error.message
+      );
+      throw new InternalServerErrorException("Failed to load country data");
+    }
   }
 }
