a99ded19c19a78f0da72b47f4e758c984fca8a0a
DmytrivDev <151439615+DmytrivDev@users.noreply.github.com>
2025-10-12 17:46:00 +0300

    Seo Meta List

--- src/modules/seo-meta/seo-meta.service.ts
+++ src/modules/seo-meta/seo-meta.service.ts
@@ -12,7 +12,7 @@ export class SeoMetaService {
     if (page === "home") {
       collection = `${page}-seo-meta`; // home-seo-meta, about-seo-meta
     } else if (page === "AmbassadorsList") {
-      collection = `ambassadors-list-seo-meta`;
+      collection = `ambassadors-list-page`;
     } else {
       collection = `${page}-page`;
     }
