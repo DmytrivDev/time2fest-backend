768618bd7a3890d9e3c683c10882ebf8cbf357ad
DmytrivDev <151439615+DmytrivDev@users.noreply.github.com>
2025-10-14 16:30:59 +0300

    фівфі

--- src/modules/sitemap/sitemap.module.ts
+++ src/modules/sitemap/sitemap.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { SitemapController } from './sitemap.controller';
 import { SitemapService } from './sitemap.service';
 import { StrapiModule } from '../strapi/strapi.module';
+import { AmbassadorsListModule } from '../ambassadors-list/ambassadors-list.module'; // ✅ додаємо
 
 @Module({
-  imports: [StrapiModule],
+  imports: [
+    StrapiModule,           // модуль для StrapiService
+    AmbassadorsListModule,  // ✅ модуль, який експортує AmbassadorsListService
+  ],
   controllers: [SitemapController],
   providers: [SitemapService],
 })
