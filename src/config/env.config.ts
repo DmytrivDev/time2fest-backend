e3146668ef67fb960b147047b1deb1f3e6d0f40b
DmytrivDev <151439615+DmytrivDev@users.noreply.github.com>
2025-10-15 18:33:41 +0300

    DB test

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -5,8 +5,8 @@ export const env = {
   db: {
     host: process.env.DB_HOST,
     port: parseInt(process.env.DB_PORT || '5432'),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
     database_core: process.env.DB_NAME,
   },
 };
