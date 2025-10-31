43b63c67d9bdee1af5e1850306d9466854aa7dca
DmytrivDev <151439615+DmytrivDev@users.noreply.github.com>
2025-10-31 13:41:40 +0200

    Fix

--- src/modules/countries/countries.module.ts
+++ src/modules/countries/countries.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
-import { CountriesService } from './countries.service';
-import { CountriesController } from './countries.controller';
-import { StrapiService } from '../../services/strapi.service';
+import { Module } from "@nestjs/common";
+import { CountriesService } from "./countries.service";
+import { CountriesController } from "./countries.controller";
+import { StrapiModule } from "../strapi/strapi.module"; // краще імпортувати StrapiModule, а не сам сервіс
 
 @Module({
-  providers: [CountriesService, StrapiService],
+  imports: [StrapiModule],
+  providers: [CountriesService],
   controllers: [CountriesController],
+  exports: [CountriesService],
 })
 export class CountriesModule {}
